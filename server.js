9df5a259f8cd43d27415a1127ddae15f52aa194b
mateusrcm <mateus.rc.martins@gmail.com>
2024-05-17 14:37:59 -0300

    routes for saving products with images

--- server.js
+++ server.js
@@ -3,26 +3,71 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
-
-// Add custom routes before JSON Server router
-server.get("/echo", (req, res) => {
-	res.jsonp(req.query);
-});
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-	if (req.method === "POST") {
-		req.body.createdAt = Date.now();
+
+server.post("/products_with_images", (req, res, next) => {
+	const resource = update(req);
+
+	if (resource) {
+		res.locals.data = resource;
 	}
-	// Continue to JSON Server router
+
 	next();
 });
 
-// Use default router
+server.patch("/products_with_images/:id", (req, res, next) => {
+	const resource = update(req);
+
+	if (resource) {
+		res.locals.data = resource;
+	}
+
+	next();
+});
+
+server.put("/products_with_images/:id", (req, res, next) => {
+	const resource = update(req);
+
+	if (resource) {
+		res.locals.data = resource;
+	}
+
+	next();
+});
+
+function update(req) {
+	const images = req.body.product_images;
+
+	delete req.body.product_images;
+
+	const product = insert(req, req.body, "products"); // Add product
+
+	product.product_images = images.map((image) => {
+		return insert(req, { productId: product.id, ...image }, "product_images"); // Add product images
+	});
+
+	return product;
+}
+
+function insert(req, data, name) {
+	const db = router.db; // Assign the lowdb instance
+	let chain = db.get(name);
+
+	if (data.id) {
+		const chainByMethod = {
+			PATCH: chain.updateById(data.id, data),
+			PUT: chain.replaceById(data.id, data),
+		};
+
+		chain = chainByMethod[req.method];
+	} else {
+		chain = chain.insert(data);
+	}
+
+	return chain.value();
+}
+
 server.use(router);
 server.listen(3000, () => {
 	console.log("JSON Server is running");
